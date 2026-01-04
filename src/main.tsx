bcd4903c4462d494e37172be27dbc75074aa3bda
omerf130 <ofaclieru@gmail.com>
2026-01-04 20:54:29 +0200

    ga4 added

--- src/main.tsx
+++ src/main.tsx
@@ -10,10 +10,16 @@ import App from "./App.tsx";
 import "./index.scss";
 import "./assets/styles/main.scss";
 import Projects from "./pages/projects/Projects.tsx";
+import ReactGA from "react-ga4";
+import GATracker from "./components/GATracker/GATracker.tsx";
+
+ReactGA.initialize("G-9G2M3T5KEG");
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+    <GATracker/>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/projects" element={<Projects />} />
