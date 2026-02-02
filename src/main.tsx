bbed3db2566d17da75df8ecc0fbe49ac638e7e4a
omerf130 <ofaclieru@gmail.com>
2026-02-02 21:50:49 +0200

    analytica

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,11 @@ import Projects from "./pages/projects/Projects.tsx";
 import ReactGA from "react-ga4";
 import GATracker from "./components/GATracker/GATracker.tsx";
 
+// Initialize Google Analytics
 ReactGA.initialize("G-9G2M3T5KEG");
 
+// Track initial page view
+ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
