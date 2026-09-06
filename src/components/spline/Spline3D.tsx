060af24d4fc7276321fb050496bc1cdeb2ca411a
omerf130 <ofaclieru@gmail.com>
2026-09-06 12:07:07 +0300

    checkpoint E4.1: remove unused 3D dependencies

--- src/components/spline/Spline3D.tsx
+++
@@ -1,8 +0,0 @@
-import Spline from '@splinetool/react-spline';
-import "./Spline.scss"
-
-export default function Spline3D() {
-  return (
-    <Spline className='spline' scene="https://prod.spline.design/MeErLcEswSW2Vp5I/scene.splinecode" />
-  );
-}
